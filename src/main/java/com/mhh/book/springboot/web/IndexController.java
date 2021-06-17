bd1b0b4166888ca16b619e12b6b49e8e717e7570
HyunHoMoon <44826442+gusgh9176@users.noreply.github.com>
2021-06-17 22:15:46 +0900

    게시글 작성시 작성자 로그인한 아이디로 고정

--- src/main/java/com/mhh/book/springboot/web/IndexController.java
+++ src/main/java/com/mhh/book/springboot/web/IndexController.java
@@ -29,7 +29,10 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(Model model, @LoginUser SessionUser user) {
+        if(user != null) {
+            model.addAttribute("author", user.getName());
+        }
         return "posts-save";
     }
 
